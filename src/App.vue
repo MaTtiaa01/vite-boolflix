d7d12dc42d60e42c53117b040bb508101a8a5bbf
MaTtiaa01 <mattia2001@live.it>
2022-11-17 12:52:53 +0100

    change v-if in li

--- src/App.vue
+++ src/App.vue
@@ -28,7 +28,7 @@ export default {
 <template>
   <header>
     <div class="form_control">
-      <input v-model="store.queryInput" type="text" placeholder="Serach movie">
+      <input v-model="store.queryInput" type="text" placeholder="Serach movie" @keyup.enter="store.callApi()">
       <button @click.prevent="store.callApi()">click</button>
     </div>
   </header>
@@ -40,7 +40,7 @@ export default {
         <li v-else-if="movie.original_language === 'it'"><img src="./assets/img/flag-italy.svg.png" alt=""></li>
         <li v-else> {{ movie.original_language }} </li>
         <li>{{ movie.original_title }} {{ movie.original_name }}</li>
-        <li v-for="n in voteTransform(movie.vote_average)">
+        <li v-if="movie.vote_average !== ''" v-for="n in voteTransform(movie.vote_average)">
           <font-awesome-icon icon="fa-solid fa-star" />
         </li>
         <li><img :src="store.posterImgUrl + movie.poster_path" alt=""></li>
